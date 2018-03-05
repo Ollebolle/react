6f516c480a243a8851a5771eec95a2975542cb27
Ollebolle <ollesv@gmail.com>
2018-03-05 18:27:50 +0100

    Changed strange home component name

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './home.sass'
 
+export default class Home extends Component {
 
-export default class Live extends Component {
   render() {
     return (
       <div>
